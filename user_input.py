3f820a070915d1acbeb2fb9d00369e358f9a5057
Blessing Jimoh <114549361+Bless-i@users.noreply.github.com>
2024-11-10 15:10:20 +0100

    commit

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# ask the user for their name
+Name = input("What is your name?")
+
+#ask the user for their age
+Age = input("what is your age?")
+
+# ask the user for their location
+Location = input("where do you live?")
+
+print(f"Hello{Name},you are {Age} years old and live in {Location}")
